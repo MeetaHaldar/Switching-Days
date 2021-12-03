d6b1845e544477abf43121d0bc6e4b54f4868b95
meeta haldar <meetahaldar1001@gmail.com>
2021-12-03 19:41:54 +0530

    completed

---
+++ index.js
@@ -0,0 +1,39 @@
+let btn =document.getElementById("toggle");
+let box =document.getElementById("box");
+let day =document.getElementById("day");
+let heading =document.getElementById("heading");
+let showingDate =document.getElementById("showingDate");
+
+
+
+
+btn.addEventListener("click" , toggleButton)
+function toggleButton(e)
+{
+if(btn.textContent.includes('night'))
+{
+    day.src="images/night.png";
+    btn.textContent="switch to day";
+    box.classList.add("night"); 
+ heading.textContent="Goodnight !!"
+ 
+}
+else{
+        day.src="images/day.png";
+    btn.textContent="switch to night";
+     box.classList.remove("night");
+    box.classList.add("day");
+     heading.textContent="Goodmorning !!"
+
+}
+}
+
+
+var today = new Date();
+
+var date = today.getFullYear()+'/'+(today.getMonth()+1)+'/'+today.getDate();
+
+var time = today.getHours() + ":" + today.getMinutes();
+var dateTime = date+' '+time;
+
+showingDate.textContent=dateTime;
